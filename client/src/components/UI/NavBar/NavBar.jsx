d2f3413facf3381349e9a4558cc4fa7641541398
Laura Kappa <laurakappa@Lauras-MacBook-Pro.local>
2022-02-25 13:51:28 -0500

    added clickable links to navbar for login, register, made title clickable to return user to homepage

--- client/src/components/UI/NavBar/NavBar.jsx
+++ client/src/components/UI/NavBar/NavBar.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 
-export default function () {
+export default function (props) {
   return (
     <div>
+      <Link to='/'>
       <h1>Party Finder</h1>
+      </Link>
+      {props.CurrentUser ?
       <div>
-        current user stuff
-      </div>
+      <h4>Welcome, {props.currentUser.username}!</h4>
+      <button onClick={props.logout}>Logout</button>
+        </div>
+        :
+        <div>
+          <Link to='/login'>Login</Link>
+          <Link to='/register'>Register</Link>
+        </div>
+    }
     </div>
   )
 }
+
+
