9944263de7013f60bfc520903f852e2b2c54de05
Laura Kappa <laurakappa@Lauras-MacBook-Pro.local>
2022-02-25 14:43:02 -0500

    created login form

--- client/src/components/User/Login/Login.jsx
+++ client/src/components/User/Login/Login.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { loginUser } from '../../../services/user';
+
 
 export default function Login() {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
   return (
-    <div>Login</div>
+    <div>
+      <form>
+        <label>Username</label>
+        <input
+          required
+          autoFocus
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button>Login</button>
+      </form>
+    </div>
   )
 }
