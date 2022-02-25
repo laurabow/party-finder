ac862abce12d5c531ec77fafe33f30bb344b55c4
Laura Kappa <laurakappa@Lauras-MacBook-Pro.local>
2022-02-25 11:59:12 -0500

    made functions for post service file

---
+++ client/src/services/posts.js
@@ -0,0 +1,26 @@
+import { api } from "./api-helper";
+
+export const getAllPosts = async () => {
+  const resp = await api.get('/posts')
+  return resp.data
+}
+
+export const getOnePost = async (post_id) => {
+  const resp = await api.get(`/posts/${post_id}`)
+  return resp.data
+}
+
+export const createPost = async (postData) => {
+  const resp = await api.post('/posts', {post: postData})
+  return resp.data
+}
+
+export const updatePost = async (post_id, postData) => {
+  const resp = await api.put(`/posts/${post_id}`, {post: postData})
+  return resp.data
+}
+
+export const deletePost = async (post_id) => {
+  const resp = await api.delete(`/posts/${post_id}`)
+  return resp.data
+}
