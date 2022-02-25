bcb3a7896d8f5ffea666dc77479e1a39557666f3
Laura Kappa <laurakappa@Lauras-MacBook-Pro.local>
2022-02-25 15:05:18 -0500

    found the typo in navbar, started working on registration form

--- client/src/components/UI/NavBar/NavBar.jsx
+++ client/src/components/UI/NavBar/NavBar.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 
-export default function (props) {
+export default function NavBar(props) {
   return (
     <div>
       <Link to='/'>
       <h1>Party Finder</h1>
       </Link>
-      {props.CurrentUser ?
       <div>
-      <h4>Welcome, {props.currentUser.username}!</h4>
-      <button onClick={props.logout}>Logout</button>
-        </div>
-        :
-        <div>
-          <Link to='/login'>Login</Link>
-          <Link to='/register'>Register</Link>
-        </div>
-    }
+        {props.currentUser ?
+        <>
+        <h4>Welcome, {props.currentUser.username}!</h4>
+        <button onClick={props.logout}>Logout</button>
+          </>
+          :
+          <>
+            <Link to='/login'>Login</Link>
+            <Link to='/register'>Register</Link>
+          </>
+          }
+      </div>
     </div>
   )
 }
--- client/src/components/User/Register/Register.jsx
+++ client/src/components/User/Register/Register.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { useState } from 'react';
+import { registerUser } from '../../../services/user';
+import { useNavigate } from 'react-router-dom';
 
 export default function Register() {
+
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+  const navigate = useNavigate();
+
   return (
-    <div>Register</div>
+    <div>
+      <form>
+        <label>Username</label>
+        <input
+          
+        />
+      </form>
+    </div>
   )
 }
