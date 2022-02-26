d6c5817b27612d6547b953d66478747b174aad48
Laura Kappa <laurakappa@Lauras-MacBook-Pro.local>
2022-02-26 15:22:13 -0500

    started some basic styling to navbar

--- client/src/components/UI/NavBar/NavBar.jsx
+++ client/src/components/UI/NavBar/NavBar.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
+import './NavBar.css';
 import { Link } from "react-router-dom";
 
 export default function NavBar(props) {
   return (
-    <div>
+    <div className='navbar'>
       <Link to='/'>
       <h1>Party Finder</h1>
       </Link>
-      <div>
+      <div className='welcome-buttons'>
         {props.currentUser ?
-        <>
+        <div className='welcome-buttons'>
             <h4>Welcome, {props.currentUser.username}!</h4>
             <Link to='/posts'><button>Find a Party!</button></Link>
             <Link to='/posts/create'><button>Create a Party!</button></Link>
         <button onClick={props.logout}>Logout</button>
-          </>
+          </div>
           :
           <>
             <Link to='/login'>Login</Link>
