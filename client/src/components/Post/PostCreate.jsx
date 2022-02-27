0da0e5beb93d7dd8bcf191ceb063f88508d5dcfe
Laura Kappa <laurakappa@Lauras-MacBook-Pro.local>
2022-02-27 09:26:36 -0500

    the dang button was outside the form on postcreate, moved the button into form, it now works perfectly, added layout to post create and posts components

--- client/src/components/Post/PostCreate.jsx
+++ client/src/components/Post/PostCreate.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Layout from '../UI/Layout/Layout';
 
 
 let days = [
@@ -24,57 +25,59 @@ export default function PostCreate(props) {
   };
 
   return (
-    <div>
-      <form onSubmit={(e) => {
-        e.preventDefault()
-        const post = {
-          title,
-          game_system,
-          day,
-          // time,
-          description
-        }
-        props.handleCreate(post);
-      }}>
-        <label>Party Name</label>
-        <input
-          required
-          autoFocus
-          type="text"
-          value={title}
-          placeholder="Name your Party!"
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <label>Game System</label>
-        <input
-          required
-          type="text"
-          value={game_system}
-          placeholder="Dungeons and Dragons"
-          onChange={(e) => setGameSystem(e.target.value)}
-        />
-        <label>Day</label>
-        <select onChange={handleDateChange}>
-          <option value={day}> -- Select a Day -- </option>
-          {days.map(({ key, day }) => (
-            <option key={key} value={key}>
-              {day}
-            </option>
-          ))}
-        </select>
-        <label>Time</label>
-        <p>figure out how to do time!</p>
-        <label>Description</label>
-        <textarea
-          required
-          type="text"
-          value={description}
-          placeholder="Describe your game and party"
-          onChange={(e) => setDescription(e.target.value)}
-        />
-      </form>
-      <button>Create Party Post</button>
-    </div>
+    <Layout>
+      <div>
+        <form onSubmit={(e) => {
+          e.preventDefault()
+          const post = {
+            title,
+            game_system,
+            day,
+            // time,
+            description
+          }
+          props.handleCreate(post);
+        }}>
+          <label>Party Name</label>
+          <input
+            required
+            autoFocus
+            type="text"
+            value={title}
+            placeholder="Name your Party!"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <label>Game System</label>
+          <input
+            required
+            type="text"
+            value={game_system}
+            placeholder="Dungeons and Dragons"
+            onChange={(e) => setGameSystem(e.target.value)}
+          />
+          <label>Day</label>
+          <select onChange={handleDateChange}>
+            <option value={day}> -- Select a Day -- </option>
+            {days.map(({ key, day }) => (
+              <option key={key} value={key}>
+                {day}
+              </option>
+            ))}
+          </select>
+          <label>Time</label>
+          <p>figure out how to do time!</p>
+          <label>Description</label>
+          <textarea
+            required
+            type="text"
+            value={description}
+            placeholder="Describe your game and party"
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <button>Create Party Post</button>
+        </form>
+      </div>
+    </Layout>
   )
 }
 
--- client/src/components/Post/Posts.jsx
+++ client/src/components/Post/Posts.jsx
@@ -1,27 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import Layout from '../UI/Layout/Layout';
 
 // make post card and replace post details with the card component
 
 export default function Posts(props) {
   return (
-    <div>
-      <h1>Find a Party!</h1>
-      {
-        props.currentUser &&
-        <Link to='/posts/create'><button>Create a Party!</button></Link>
-      }
-      {
-        props.posts.map(post => (
-          <Link key={post.id} to={`/posts/${post.id}`}>
-            <h2>{post.title}</h2>
-            {/* <h3>{post.user.username}</h3> */}
-            <h3>{post.game_system}</h3>
-            <h3>{post.day} at TIME</h3>
-            <p>{post.description}</p>
-          </Link>
-        ))
-      }
-    </div>
+    <Layout>
+      <div>
+        <h1>Find a Party!</h1>
+        {
+          props.currentUser &&
+          <Link to='/posts/create'><button>Create a Party!</button></Link>
+        }
+        {
+          props.posts.map(post => (
+            <Link key={post.id} to={`/posts/${post.id}`}>
+              <h2>{post.title}</h2>
+              {/* <h3>{post.user.username}</h3> */}
+              <h3>{post.game_system}</h3>
+              <h3>{post.day} at TIME</h3>
+              <p>{post.description}</p>
+            </Link>
+          ))
+        }
+      </div>
+    </Layout>
   )
 }
