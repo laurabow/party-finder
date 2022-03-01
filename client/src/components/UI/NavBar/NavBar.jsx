ff61200382d94551503af74a2787b530c12a5f0d
Laura Kappa <laurakappa@Lauras-MacBook-Pro.local>
2022-03-01 10:48:18 -0500

    styled buttons on navbar

--- client/src/components/UI/NavBar/NavBar.jsx
+++ client/src/components/UI/NavBar/NavBar.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 import './NavBar.css';
 import { Link } from "react-router-dom";
 import { D20 } from '../../../assets/index.js';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+
+const StyledTooltip = styled(({ className, ...props }) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))`
+  & .MuiTooltip-tooltip {
+    background: navy;
+  }
+`;
 
 export default function NavBar(props) {
   return (
@@ -18,15 +29,52 @@ export default function NavBar(props) {
         {props.currentUser ?
         <div className='welcome-buttons'>
             <h4>Welcome, {props.currentUser.username}!</h4>
-            <Link to='/posts'><button>Find a Party!</button></Link>
-            <Link to='/posts/create'><button>Create a Party!</button></Link>
-        <button onClick={props.logout}>Logout</button>
+            <Link to='/posts'>
+              <StyledTooltip title="Find a Party">
+                  <Button variant="contained" color="primary">
+                    Find a Party!
+                  </Button>
+                </StyledTooltip>
+            </Link>
+            <Link to='/posts/create'>
+              <StyledTooltip title="Create a Party">
+                  <Button variant="contained" color="primary">
+                    Create a Party!
+                  </Button>
+                </StyledTooltip>
+            </Link>
+              <StyledTooltip title="Logout">
+                <Button
+                  onClick={props.logout}
+                  variant="contained"
+                  color="primary">
+                    Logout
+                  </Button>
+                </StyledTooltip>
           </div>
           :
           <>
-            <Link to='/posts'><button>Find a Party!</button></Link>
-            <Link to='/login'><button>Login</button></Link>
-            <Link to='/register'><button>Register</button></Link>
+            <Link to='/posts'>
+              <StyledTooltip title="Find a Party">
+                  <Button variant="contained" color="primary">
+                    Find a Party!
+                  </Button>
+                </StyledTooltip>
+            </Link>
+            <Link to='/login'>
+              <StyledTooltip title="Login">
+                  <Button variant="contained" color="primary">
+                    Login
+                  </Button>
+                </StyledTooltip>
+            </Link>
+            <Link to='/register'>
+              <StyledTooltip title="Register">
+                  <Button variant="contained" color="primary">
+                    Register
+                  </Button>
+                </StyledTooltip>
+            </Link>
           </>
           }
       </div>
