f454c623d5571f5004abab8cbda613c877b37361
Laura Kappa <laurakappa@Lauras-MacBook-Pro.local>
2022-03-01 16:19:18 -0500

    login styling

--- client/src/components/User/Login/Login.jsx
+++ client/src/components/User/Login/Login.jsx
@@ -3,6 +3,18 @@ import { useNavigate } from 'react-router-dom';
 import { loginUser } from '../../../services/user';
 import Layout from '../../UI/Layout/Layout';
 import './Login.css';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+
+
+const StyledTooltip = styled(({ className, ...props }) => (
+  <Tooltip {...props} classes={{ popper: className }} />
+))`
+  & .MuiTooltip-tooltip {
+    background: navy;
+  }
+`;
 
 
 export default function Login(props) {
@@ -14,35 +26,47 @@ export default function Login(props) {
 
   return (
     <Layout>
-      <div className='login'>
-        <form onSubmit={async (e) => {
-            e.preventDefault()
-            const user = {
-              username,
-              password
+      <div className='login-page'>
+        <h1 className='login-title'>Login</h1>
+        <div className='login-form-container'>
+          <form
+            className='login-form'
+            onSubmit={async (e) => {
+              e.preventDefault()
+              const user = {
+                username,
+                password
+              }
+              const resp = await loginUser(user)
+              props.setCurrentUser(resp)
+              navigate('/posts');
             }
-            const resp = await loginUser(user)
-            props.setCurrentUser(resp)
-            navigate('/posts');
-          }
-        }>
-          <label>Username</label>
-          <input
-            required
-            autoFocus
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <label>Password</label>
-          <input
-            required
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button>Login</button>
-        </form>
+          }>
+            <label className='login-username'>Username</label>
+            <input
+              required
+              autoFocus
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <label className='login-password'>Password</label>
+            <input
+              required
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <StyledTooltip title="Create Party">
+              <Button
+                id='login-form-btn'
+                variant="contained"
+                color="primary">
+                Login
+              </Button>
+            </StyledTooltip>
+          </form>
+        </div>
       </div>
     </Layout>
   )
