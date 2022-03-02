ef6c370d3b1f41b0bb2f34ac9c0ffa69d103ce25
Laura Kappa <laurakappa@Lauras-MacBook-Pro.local>
2022-03-02 10:38:08 -0500

    localhost

--- client/src/services/api-helper.js
+++ client/src/services/api-helper.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'https://ttrpg-party-finder.herokuapp.com/'
+const baseURL = process.env.NODE_ENV === 'production' ? 'https://ttrpg-party-finder.herokuapp.com/' : 'http://localhost:3000/'
 // update baseurl once deployed
 
 export const api = axios.create({
