73617235b65864a5f6786137414b49770d1cd752
Laura Kappa <laurakappa@Lauras-MacBook-Pro.local>
2022-03-02 09:52:01 -0500

    updated config file

--- client/src/services/api-helper.js
+++ client/src/services/api-helper.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const baseUrl = process.env.NODE_ENV === 'https://ttrpg-party-finder.herokuapp.com/' || 'http://localhost:3000/'
+const baseURL = 'https://ttrpg-party-finder.herokuapp.com/'
 // update baseurl once deployed
 
 export const api = axios.create({
-  baseURL: baseUrl
+  baseURL
 })
